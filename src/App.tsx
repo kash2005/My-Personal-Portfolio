05c1f236dd9340939acfd3048f7804acd5f2f377
kash2005 <kashmifernando3@gmail.com>
2024-08-13 14:47:15 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import Hero from './components/Hero.tsx'
+import Navbar from './components/Navbar.tsx'
+import SlidingText from './constrant/ImageSlider.tsx'
+function App() {
+
+  return (
+    <div className="w-full h-full absolute bg-white">
+      <Navbar />
+      {/* <Hero /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const Navbar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  return (
+    <header className='flex justify-between items-center text-black py-3 px-8 md:px-32 bg-transparent border-b border-neutral-900'>
+      <a href='#'>
+        <img src="./src/assets/logo.png" alt='' className='w-57 h-[33] hover:scale-105 transition-all'></img>
+      </a>  
+      <ul className='hidden xl:flex items-center gap-9  text-base'>
+        <li className='p-3 text-[#001845] hover:underline hover:bg-[#001845] hover:text-white rounded-md transition-all cursor-pointer'>Home</li>
+        <li className='p-3 text-[#001845] hover:underline hover:bg-[#001845] hover:text-white rounded-md transition-all cursor-pointer'>About me</li>
+        <li className='p-3 text-[#001845] hover:underline hover:bg-[#001845] hover:text-white rounded-md transition-all cursor-pointer'>Services</li>
+        <li className='p-3 text-[#001845] hover:underline hover:bg-[#001845] hover:text-white rounded-md transition-all cursor-pointer'>Projects</li>
+        <li className='p-3 text-[#001845] hover:underline hover:bg-[#001845] hover:text-white rounded-md transition-all cursor-pointer'>Contact me</li>
+      </ul>
+      <button className="relative hidden md:flex items-center justify-center gap-3 px-4 py-2 bg-white text-[#001845] font-semibold rounded-lg hover:bg-[#001845] hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-300">view my work</button>
+      
+      <i className='bx bx-menu xl:hidden block text-5xl cursor-pointer' onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+
+      <div className={`absolute xl:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${isMenuOpen ? "opacity-100" : "opacity-0"}`}
+
+      style={{transition:"transform 0.3 ease,opacity 0.3s ease"}}
+      >
+        <li className='list-none w-full text-center p-4 hover:bg-[#001845] hover:text-white transition-all cursor-pointer'>Home</li>
+        <li className='list-none w-full text-center p-4 hover:bg-[#001845] hover:text-white transition-all cursor-pointer'>About me</li>
+        <li className='list-none w-full text-center p-4 hover:bg-[#001845] hover:text-white transition-all cursor-pointer'>Services</li>
+        <li className='list-none w-full text-center p-4 hover:bg-[#001845] hover:text-white transition-all cursor-pointer'>Project</li>
+        <li className='list-none w-full text-center p-4 hover:bg-[#001845] hover:text-white transition-all cursor-pointer'>Contact me</li>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar;

d76816bedb6dd94d7fcab091a2710cc34ae9ff03
kash2005 <kashmifernando3@gmail.com>
2024-08-13 17:01:04 +0530

    add left side content in hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import Hero from './components/Hero.tsx'
+import Hero from './components/Hero.tsx'
 import Navbar from './components/Navbar.tsx'
 import SlidingText from './constrant/ImageSlider.tsx'
 function App() {
